e5a4243e9cf17b152b08bdb4e72194cb3cfb2d34
dedlyme <k.vasarajs@gmail.com>
2025-10-20 14:45:20 +0300

    bootstrap.js
    dwad

--- resources/js/bootstrap.js
+++ resources/js/bootstrap.js
@@ -6,10 +6,6 @@ window.Pusher = Pusher;
 window.Echo = new Echo({
     broadcaster: 'pusher',
     key: import.meta.env.VITE_PUSHER_APP_KEY,
-    cluster: import.meta.env.VITE_PUSHER_APP_CLUSTER ?? 'eu',
-    wsHost: `ws-${import.meta.env.VITE_PUSHER_APP_CLUSTER}.pusher.com`,
-    wsPort: 80,
-    wssPort: 443,
-    forceTLS: false,
-    enabledTransports: ['ws', 'wss'],
+    cluster: import.meta.env.VITE_PUSHER_APP_CLUSTER,
+    forceTLS: true,
 });
